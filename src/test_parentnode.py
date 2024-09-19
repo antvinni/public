f879217a7e368a7679310dcf141142d1fcf61870
antvinni <ant.vinni@gmail.com>
2024-09-19 13:45:00 +0100

    updated tests for ParentNode

--- src/test_parentnode.py
+++ src/test_parentnode.py
@@ -18,6 +18,19 @@ class TestParentNode(unittest.TestCase):
             node.to_html(), 
             '<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>'
         )
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode("a", "Normal text", {"key1":"value1", "key2":"value2"}),
+            ],
+        )
+        self.assertEqual(
+            node.to_html(), 
+            '<p><b>Bold text</b>Normal text<i>italic text</i><a key1="value1" key2="value2">Normal text</a></p>'
+        )
 
 if __name__ == "__main__":
     unittest.main()
